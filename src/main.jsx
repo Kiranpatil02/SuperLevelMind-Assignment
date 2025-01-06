e2f5c44f83aa1967233d323239f3d25b197f8798
Kiran <Kiranpatil73827@gmail.com>
2025-01-06 22:42:17 +0530

    Router config

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { createBrowserRouter,RouterProvider } from 'react-router-dom'
 import App from './App.jsx'
+import Layout from './Components/Bot/Layout.jsx'
+
+const router=createBrowserRouter([
+  {
+    path:"/",
+    element:<App/>,
+  },
+  {
+    path:"/bot",
+    element:<Layout/>
+  }
+])
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
